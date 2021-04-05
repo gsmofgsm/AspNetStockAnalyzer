e25f8bc528d049ad5ffdc2d9cbd8e8e95e7b2620
Qing Ma <q.ma@ncontrol.cloud>
2021-04-05 16:48:36 +0200

    Make search async

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace StockAnalyzer.Windows
             InitializeComponent();
         }
 
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private async void Search_Click(object sender, RoutedEventArgs e)
         {
             #region Before loading stock data
             var watch = new Stopwatch();
@@ -27,13 +27,16 @@ namespace StockAnalyzer.Windows
             StockProgress.IsIndeterminate = true;
             #endregion
 
-            var client = new WebClient();
+            using (var client = new HttpClient()) 
+            {
+                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
 
-            var content = client.DownloadString($"http://localhost:61363/api/stocks/{Ticker.Text}");
+                var content = await response.Content.ReadAsStringAsync();
 
-            var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+                var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
 
-            Stocks.ItemsSource = data;
+                Stocks.ItemsSource = data;
+            }
 
             #region After stock data is loaded
             StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
