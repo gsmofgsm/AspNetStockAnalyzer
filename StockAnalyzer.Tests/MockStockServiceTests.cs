8fa25639b667ddc33e46301f0a85498f611a8696
Qing Ma <q.ma@ncontrol.cloud>
2021-04-06 12:33:20 +0200

    MockStockServiceTests

---
+++ StockAnalyzer.Tests/MockStockServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockAnalyzer.Windows.Services;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockAnalyzer.Tests
+{
+    [TestClass]
+    public class MockStockServiceTests
+    {
+        [TestMethod]
+        public async Task Can_Load_All_MSFT_StocksAsync()
+        {
+            var service = new MockStockService();
+            var stocks = await service.GetStockPricesFor("MSFT",
+                CancellationToken.None);
+
+            Assert.AreEqual(stocks.Count(), 2);
+            Assert.AreEqual(stocks.Sum(stock => stock.Change), 0.7m);
+        }
+    }
+}
---
+++ StockAnalyzer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StockAnalyzer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StockAnalyzer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b95ad37-ecfe-4630-a0f5-1558fa65c752")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
